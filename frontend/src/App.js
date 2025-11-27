8652be0ddc9b97f385e51c2e01617bf76b262cc2
Mansha Sanger <manshasanger@Manshas-MacBook-Air.local>
2025-11-27 11:58:13 +0530

    Initial commit: Add backend, frontend, and README

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  return (
+    <div>
+      {!user ? <Login setUser={setUser} /> : <Dashboard />}
+    </div>
+  );
+}
+
+export default App;
